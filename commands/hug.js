4601961b7dc7124dcac4b82f3b8833e26009b489
eXway <r.okenczyc123@gmail.com>
2018-06-13 18:02:48 +0200

    4537

--- commands/hug.js
+++ commands/hug.js
@@ -3,13 +3,14 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
-
+let hUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!hUser) let hUser = "**siebie**";
 let {body} = await superagent
-.get(`http://api.giphy.com/v1/gifs/random?api_key=fsreqhxonBj4YypDt0UjdgHGpirrTa1k&s=hug&fmt=json`);
+.get(`http://api.giphy.com/v1/gifs/random?api_key=fsreqhxonBj4YypDt0UjdgHGpirrTa1k&tag=hug&fmt=json`);
 
 let hugembed = new Discord.RichEmbed()
-.setColor("pink")
-.setTitle("HUG")
+.setColor("#8f8ff1")
+.setTitle(`${message.author} przytulił ${hUser}`)
 .setImage(body.data.images.original.url);
 
 message.channel.send(hugembed);
