cc14031caaf06cd8c8862353db764638300a1956
eXway <r.okenczyc123@gmail.com>
2018-06-13 18:59:45 +0200

    couple-hug

--- commands/hug.js
+++ commands/hug.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 let hUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!hUser) return message.channel.send("Nie podałeś kogo chcesz przytulić :c")
 let {body} = await superagent
-.get(`http://api.giphy.com/v1/gifs/random?api_key=fsreqhxonBj4YypDt0UjdgHGpirrTa1k&tag=hug&fmt=json`);
+.get(`http://api.giphy.com/v1/gifs/random?api_key=fsreqhxonBj4YypDt0UjdgHGpirrTa1k&tag=couple-hug&fmt=json`);
 
 let hugembed = new Discord.RichEmbed()
 .setColor("#dda6bb")
