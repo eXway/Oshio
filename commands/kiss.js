d86f913e64f561e369b969fe436297f88b6a8550
eXway <38926822+eXway@users.noreply.github.com>
2018-06-15 11:17:25 +0200

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -3,13 +3,13 @@ const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
 let hUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if(!hUser) return message.channel.send("Nie podałeś kogo chcesz przytulić :c");
+if(!hUser) return message.channel.send("Nie podałeś kogo chcesz pocałować :c");
 let {body} = await superagent
 .get(`https://api.tenor.com/v1/random?key=88RODP3HJCA2&q=Anime+kiss&safesearch=off`);
 
 let hugembed = new Discord.RichEmbed()
 .setColor("#dda6bb")
-.addField("Hug!", `${message.author} przytulił(a) ${hUser}`)
+.addField("OMG <3!", `${message.author} pocałował(a) ${hUser}`)
 .setImage(body.results[0].media[0].gif.url);
 
 message.delete().catch(O_o=>{});
