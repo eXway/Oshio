982160382e19057a0346048921b622474ee918d6
eXway <38926822+eXway@users.noreply.github.com>
2018-06-15 11:09:47 +0200

    Create kiss.js

---
+++ commands/kiss.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+let hUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!hUser) return message.channel.send("Nie podałeś kogo chcesz przytulić :c");
+let {body} = await superagent
+.get(`https://api.tenor.com/v1/random?key=88RODP3HJCA2&q=Anime+kiss&safesearch=off`);
+
+let hugembed = new Discord.RichEmbed()
+.setColor("#dda6bb")
+.addField("Hug!", `${message.author} przytulił(a) ${hUser}`)
+.setImage(body.results[0].media[0].gif.url);
+
+message.delete().catch(O_o=>{});
+
+message.channel.send(hugembed);
+
+}
+
+module.exports.help = {
+  name: "kiss"
+}
