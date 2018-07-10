46797f10f27617f3cfbf7c1ffc282e1533edf5eb
eXway <38926822+eXway@users.noreply.github.com>
2018-07-10 12:43:00 +0200

    Nowy komenda - kotek <3

---
+++ commands/cat.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+
+let hugembed = new Discord.RichEmbed()
+.setColor("#dda6bb")
+.addField("Kotek! <3`)
+.setImage(http://thecatapi.com/api/images/get?format=xml);
+
+message.channel.send(hugembed);
+
+}
+
+module.exports.help = {
+  name: "cat"
+}
