6e3e849cf11296fad83372c60e429820bfd0814e
eXway <38926822+eXway@users.noreply.github.com>
2018-07-10 12:53:30 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
 let catembed = new Discord.RichEmbed()
 .setColor("#dda6bb")
-.addField("Kotek! <3")
+.addField("Kotek! <3", "Darmowy słodziak dla ciebie!)
 .setImage("http://thecatapi.com/api/images/get?format=src");
 
 message.channel.send(catembed);
