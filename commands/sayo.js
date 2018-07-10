1f224d6a8f0626727991a0830049fa69ba03ec50
eXway <38926822+eXway@users.noreply.github.com>
2018-07-10 14:37:17 +0200

    Create sayo.js

---
+++ commands/sayo.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => { 
+  if(!message.member.roles.some(r=>["Administrator HO", "Administrator Discord"].includes(r.name)) || (message.author == !215420153221873665) )
+  return message.reply("Nie możesz użyć tej komendy");
+  
+  let text = args.slice(1).join(" ");
+  message.delete();
+  message.channel.send(text);
+
+}
+
+module.exports.help = {
+  name: "cat"
+}
