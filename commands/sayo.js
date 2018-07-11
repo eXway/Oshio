c8c6680a115d06ad96ff2b11978b5b33b2d56217
eXway <38926822+eXway@users.noreply.github.com>
2018-07-11 11:31:32 +0200

    Update sayo.js

--- commands/sayo.js
+++ commands/sayo.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {    
     
-      if(!message.member.hasPermission(["Administrator HO", "Administrator Discord"]) || (message.author.id == !215420153221873665) return message.channel.send("NIE MASZ PRAW!");
+      if(!message.member.hasPermission(["Administrator HO", "Administrator Discord"]) || (message.author.id == !215420153221873665) return;
       const sayMessage = args.join(" ");
 
       message.delete().catch();
-      message.channel.send(sayMessage);
+      return message.channel.send(sayMessage);
 }
 
 module.exports.help = {
