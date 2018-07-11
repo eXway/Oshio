2b2ef085a17eb0c4c809ad40f240d409bbcb3933
eXway <38926822+eXway@users.noreply.github.com>
2018-07-11 11:21:10 +0200

    Update sayo.js

--- commands/sayo.js
+++ commands/sayo.js
@@ -3,11 +3,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => { 
   if(!message.member.roles.some(r=>["Administrator HO", "Administrator Discord"].includes(r.name)) && (message.author =! 215420153221873665) )
-  return message.reply("Nie możesz użyć tej komendy");
+
+    
+    
+      if(!message.member.hasPermission(["Administrator HO", "Administrator Discord"]) || (message.author.id == !215420153221873665 ) return;
   
-  let text = args.slice(1).join(" ");
-  message.delete();
-  message.channel.send(text);
+      const sayMessage = args.join(" ");
+
+      message.delete().catch();
+      message.channel.send(sayMessage);
 
 }
 
