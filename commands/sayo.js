26dff9fe8c01929eec9980d0b54541327ade09cd
eXway <38926822+eXway@users.noreply.github.com>
2018-07-11 11:01:07 +0200

    Update and rename sayo to sayo.js

--- commands/sayo.js
+++ commands/sayo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => { 
-  if(!message.member.roles.some(r=>["Administrator HO", "Administrator Discord"].includes(r.name)) || (message.author <> 215420153221873665) )
+  if(!message.member.roles.some(r=>["Administrator HO", "Administrator Discord"].includes(r.name)) || (message.author =! 215420153221873665) )
   return message.reply("Nie możesz użyć tej komendy");
   
   let text = args.slice(1).join(" ");
